083aac4ab783a1157f1cabdae2e4ecbfd53a7664
bennypickard <bennypickard@gmail.com>
2021-01-09 10:37:58 -0800

    Added physics, movement to mouse/background
    Also added background movement/tesselation.
    Mouse collides with boundaries too.

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,6 +6,11 @@ import AnimationKeys from "/consts/AnimationKeys";
 
 export default class Game extends Phaser.Scene
 {
+	//[]Background class property
+	//The ! is TypeScript’s non-null assertion operator. This lets us tell TypeScript 
+	//that the backgroundproperty will never be undefined or null
+	private background!: Phaser.GameObjects.tileSprite;
+
 	constructor()
 	{
 		//[]INHERIT default characteristics from Phaser
@@ -31,18 +36,57 @@ export default class Game extends Phaser.Scene
 		//[]BACKGROUND
 		const width = this.scale.width;
 		const height = this.scale.height;
-		this.add.tileSprite(0,0,width,height,TextureKeys.Background)
+		this.background = this.add.tileSprite(
+			0,0,
+			width,height,
+			TextureKeys.Background
+			)
 			.setOrigin(0)
+			.setScrollFactor(0,0);//keep from scrolling, stationary to camera
+
 
 		//[]SPRITE
+		/*Basic Sprite
 		this.add.sprite(
 			width * .5,
 			height * .5,
 			TextureKeys.RocketMouse,//atlas key given in preload
 			"rocketmouse_fly01.png",//name of the frame in atlas
 		)
+		*/
+		//PhysicsSprite
+		const mouse = this.physics.add.sprite(
+			width * .5,
+			height * .5,
+			TextureKeys.RocketMouse,
+			'rocketmouse_fly01.png'
+		)
 		.play(AnimationKeys.RocketMouseRun);
+		//[]COLLISIONS
+		const body = mouse.body as Phaser.Physics.Arcade.Body
+		body.setCollideWorldBounds(true);
+		//[]VELOCITY
+		body.setVelocityX(200);
+		//[]BOUNDS
+		this.physics.world.setBounds(
+			0, 0, //x, y
+			Number.MAX_SAFE_INTEGER, height - 30 //width, height. Finitlyinfinit number
+		)
 
+		//[]CAMERA
+		this.cameras.main.startFollow(mouse);
+		this.cameras.main.setBounds(0,0,Number.MAX_SAFE_INTEGER, height);
+
+
+
+	}
+
+	update(t: number dt: number){
+	/*INPUT:  t-> total time elapsed since game startet
+			dt-> time elapsed since last frame
+	*/
+		//[]SCROLL BACKGROUND
+		this.background.setTilePosition(this.cameras.main.scrollX);
 	}
 
 	
