63d9f0edec468896bf74af4773116888c2b7dc14
bennypickard <bennypickard@gmail.com>
2021-01-28 18:18:03 -0800

    Jetpack fire, and started Cursors
    Toggled jetpack fire. Starter cursor implimentation.

--- src/game/RocketMouse.ts
+++ src/game/RocketMouse.ts
@@ -5,6 +5,10 @@ import AnimationKeys from "../consts/AnimationKeys"
 
 export default class RocketMouse extends Phaser.GameObjects.Container
 {
+	//Class properties (variables)
+	private flames: Phaser.GameObjects.sprite//required to use this.variable convention
+	private cursors: Phaser.Types.Input.Keyboard.CursorKeys
+
 	constructor(scene: Phaser.Scene, x: number, y: number)
 	{
 		super(scene, x, y)
@@ -21,15 +25,28 @@ export default class RocketMouse extends Phaser.GameObjects.Container
 		body.setOffset(mouse.width * -0.5, -mouse.height)
 
 		//[]FIRE
-		const flames = scene.add.sprite(-63,-15,TextureKeys.RocketMouse)
+		this.flames = scene.add.sprite(-63,-15,TextureKeys.RocketMouse)
 			.play(AnimationKeys.RocketFlamesOn)
+		//start flames off
+		this.enableJetpack(false);
 
 		//[]Add things to this container
-		this.add(flames)
-		this.add(mouse);
+		this.add(this.flames)
+		this.add(mouse); 
+
+		//[]CURSOR KEYS
+		this.cursors = scene.input.keyboard.createCursorKeys();
 
 	}
 
+	enableJetpack(enabled: boolean)
+	{/*INPUT: enabled, determins if flames show or not
+	This function turns the flames on and off in the 
+	jetpack. It's called from the rocketmouse contructor and also
+	Potentially from an update function.'
+	*/
+		this.flames.setVisible(enabled);
+	}
 
 
 }
